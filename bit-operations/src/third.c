1b3de904e5224862147ce43f3c5cc53f9c61d111
wildyellow <amidalo148@gmail.com>
2025-11-27 18:47:23 +0700

    Выполнение 3 задания раздела 'Битовые операции', небольшой рефакторинг 1 задания

--- bit-operations/src/third.c
+++ bit-operations/src/third.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <limits.h>
-#include <stdlib.h>
 
 int main(void)
 {
     char binary[sizeof(short) * 8 + 1] = {0};
     char shift = sizeof(short) * 8 - 1;
-    
+    short count_of_one = 0;
+
     signed int input = 0;
     unsigned short number = 0;
 
@@ -22,21 +22,25 @@ int main(void)
     if (input < 0)
     {
         printf("\nВы вышли из диапазона типа short (0-65535)!"
-               "\nВаше число %d будет преобразовано в %d из-за циклического переполнения!\n", input, input + USHRT_MAX + 1);
+               "\nВаше число %d будет преобразовано в %d из-за циклического переполнения!\n\ns", input, input + USHRT_MAX + 1);
     }
-    
 
     number = (unsigned short)input;
+    short bit = 0;
 
     short i = 0;
     for (; i <= sizeof(short) * 8 - 1; i++)
     {
-        *(binary + i) = ((number >> shift) & 1) + '0';
+        bit = (number >> shift) & 1;
+        if (bit == 1) count_of_one++;
+
+        *(binary + i) = bit + '0';
         shift--;
     }
     *(binary + i) = '\0';
 
-    printf("\nДвоичное представление: %s\n", binary);
+    printf("Двоичное представление: %s", binary);
+    printf("\nКоличество единиц: %d\n", count_of_one);
 
     return 0;
 }
